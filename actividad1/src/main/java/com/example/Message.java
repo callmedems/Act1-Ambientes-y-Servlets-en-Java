d23e87e522d719176b0f388933b023cc80924148
callmedems <[demmi.zepeda@gmail.com]>
2026-02-20 19:02:19 -0600

    message

---
+++ actividad1/src/main/java/com/example/Message.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Message {
+    private String text;
+
+    public Message(String t) { 
+        this.text = t; 
+    }
+
+    public String getText() { 
+        return text; 
+    }
+}
